d8477ada15786efda844c7b72cac7a12f63c9dcb
Rushikeshsasamkar <44942941+Rushikeshsasamkar@users.noreply.github.com>
2023-12-30 22:36:41 +0530

    NewsLetter Completed

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero/Hero';
 import Popular from '../Components/Popular/Popular';
 import Offers from '../Components/Offers/Offers';
 import NewCollections from '../Components/NewCollections/NewCollections';
+import NewsLetter from '../Components/NewsLetter/NewsLetter';
 
  const Shop = () => {
   return (
@@ -11,6 +12,7 @@ import NewCollections from '../Components/NewCollections/NewCollections';
         <Popular/>
         <Offers/>
         <NewCollections/>
+        <NewsLetter/>
     </div>
   )
 }
